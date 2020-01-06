4ee5fa1199017d59463c923b9db3c538e76d63b8
tehlordvortex <tehlordvortex@outlook.com>
2020-01-06 01:46:01 +0100

    add lists, unbounded ripples and fix formatting of material.js

--- js/material.js
+++ js/material.js
@@ -1,51 +1,64 @@
 window.mdc.autoInit();
 function initCards() {
-  var selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
-  var ripples = [].map.call(document.querySelectorAll(selector), function (el) {
+  var selector = ".mdc-button, .mdc-card__primary-action";
+  var ripples = [].map.call(document.querySelectorAll(selector), function(el) {
     return new window.mdc.ripple.MDCRipple(el);
   });
-  return ripples;
+  var unboundedSelector = ".mdc-icon-button";
+  var unboundedRipples = [].map.call(
+    document.querySelectorAll(unboundedSelector),
+    function(el) {
+      const ripple = new window.mdc.ripple.MDCRipple(el);
+      ripple.unbounded = true;
+      return ripple;
+    }
+  );
+  return ripples.concat(unboundedRipples);
 }
 function initFloatingLabels() {
-  var selector = '.mdc-floating-label';
-  var labels = [].map.call(document.querySelectorAll(selector), function (el) {
+  var selector = ".mdc-floating-label";
+  var labels = [].map.call(document.querySelectorAll(selector), function(el) {
     return new window.mdc.floatingLabel.MDCFloatingLabel(el);
   });
   return labels;
 }
 function initLineRipples() {
-  var selector = '.mdc-line-ripple';
-  var lineRipples = [].map.call(document.querySelectorAll(selector), function (el) {
+  var selector = ".mdc-line-ripple";
+  var lineRipples = [].map.call(document.querySelectorAll(selector), function(
+    el
+  ) {
     return new window.mdc.ripple.MDCRipple(el);
   });
   return lineRipples;
 }
 function initTextFields() {
-  var selector = '.mdc-text-field';
-  var textFields = [].map.call(document.querySelectorAll(selector), function (el) {
+  var selector = ".mdc-text-field";
+  var textFields = [].map.call(document.querySelectorAll(selector), function(
+    el
+  ) {
     return new window.mdc.textField.MDCTextField(el);
   });
   return textFields;
 }
 function initFABs() {
-  var selector = '.mdc-fab';
-  var fabs = [].map.call(document.querySelectorAll(selector), function (el) {
+  var selector = ".mdc-fab";
+  var fabs = [].map.call(document.querySelectorAll(selector), function(el) {
     return new window.mdc.ripple.MDCRipple(el);
   });
   return fabs;
 }
 function initTextFieldIcons() {
-  var selector = '.mdc-text-field-icon';
-  var icons = [].map.call(document.querySelectorAll(selector), function (el) {
+  var selector = ".mdc-text-field-icon";
+  var icons = [].map.call(document.querySelectorAll(selector), function(el) {
     return new window.mdc.textFieldIcon.MDCTextFieldIcon(el);
   });
   return icons;
 }
 function initSelects() {
-  var selector = '.mdc-select';
-  var selects = [].map.call(document.querySelectorAll(selector), function (el) {
+  var selector = ".mdc-select";
+  var selects = [].map.call(document.querySelectorAll(selector), function(el) {
     var select = new window.mdc.select.MDCSelect(el);
-    if (el.classList.contains('mdc-select--required')) {
+    if (el.classList.contains("mdc-select--required")) {
       select.required = true;
     }
     return select;
@@ -53,18 +66,42 @@ function initSelects() {
   return selects;
 }
 function initSelectIcons() {
-  var selector = '.mdc-select__icon';
-  var icons = [].map.call(document.querySelectorAll(selector), function (el) {
+  var selector = ".mdc-select__icon";
+  var icons = [].map.call(document.querySelectorAll(selector), function(el) {
     return new window.mdc.select.MDCSelectIcon(el);
   });
   return icons;
 }
+function initLists() {
+  var selector = ".mdc-list:not([data-mdc-auto-init])";
+  var lists = [].map.call(document.querySelectorAll(selector), function(el) {
+    return new window.mdc.list.MDCList(el);
+  });
+  const listsItemRipples = lists
+    .filter(function(list) {
+      return !list.root_.dataset.noRipple;
+    })
+    .map(function(list) {
+      return list.listElements.map(function(listItemEl) {
+        return new window.mdc.ripple.MDCRipple(listItemEl);
+      });
+    });
+  return {
+    lists: lists,
+    ripples: listsItemRipples
+  };
+}
 
-initCards();
-initFloatingLabels();
-initLineRipples();
-initTextFields();
-initTextFieldIcons();
-initFABs();
-initSelects();
-initSelectIcons();
+function mdcInitAll() {
+  initCards();
+  initFloatingLabels();
+  initLineRipples();
+  initTextFields();
+  initTextFieldIcons();
+  initFABs();
+  initSelects();
+  initSelectIcons();
+  initLists();
+}
+
+mdcInitAll();
