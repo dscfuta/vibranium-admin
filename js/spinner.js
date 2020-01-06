0e26f35b5c4a8484bcf8ceb6c20a5dce1892eeb9
tehlordvortex <tehlordvortex@outlook.com>
2020-01-06 01:47:52 +0100

    format spinner.js

--- js/spinner.js
+++ js/spinner.js
@@ -1,16 +1,20 @@
-(function ($) {
-  var spinner = $('<span></span>').addClass('small-spinner');
+(function($) {
+  var spinner = $("<span></span>").addClass("small-spinner");
   function enableLoadingMode() {
-    $('#updateButton').siblings('.mdc-button').attr('disabled', '');
-    $('#updateButton').attr('disabled', '');
-    $('#updateButton .mdc-button__label .dialog-mode').hide();
-    $('#updateButton .mdc-button__label').append(spinner);
+    $("#updateButton")
+      .siblings(".mdc-button")
+      .attr("disabled", "");
+    $("#updateButton").attr("disabled", "");
+    $("#updateButton .mdc-button__label .dialog-mode").hide();
+    $("#updateButton .mdc-button__label").append(spinner);
   }
   function disableLoadingMode() {
-    $('#updateButton .mdc-button__label').remove(spinner);
-    $('#updateButton .mdc-button__label .dialog-mode').show();
-    $('#updateButton').removeAttr('disabled');
-    $('#updateButton').siblings('.mdc-button').removeAttr('disabled');
+    spinner.remove();
+    $("#updateButton .mdc-button__label .dialog-mode").show();
+    $("#updateButton").removeAttr("disabled");
+    $("#updateButton")
+      .siblings(".mdc-button")
+      .removeAttr("disabled");
   }
   window.enableLoadingMode = enableLoadingMode;
   window.disableLoadingMode = disableLoadingMode;
